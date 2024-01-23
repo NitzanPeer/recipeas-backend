eed77c8d9ba1e8b3deaaa90d6783a6c10c911a61
Nitzan <prnitzan@gmail.com>
2024-01-23 19:24:43 +0200

    Add env variables for port and db pass

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 import mongoose from 'mongoose'
 import Express from 'express'
+import dotenv from 'dotenv'
 import { recipesRoutes } from './api/recipes/recipe.routes.js'
 import { tagsRoutes } from './api/tags/tag.routes.js'
 import cors from 'cors'
 
-
+dotenv.config()
 
 const app = Express()
-const PORT = 3030
+const PORT = process.env.PORT || 3030
 
 // const uri = 'mongodb://127.0.0.1/Recipes'
 // const uri = 'mongodb://localhost/Recipes'
-const uri = 'mongodb+srv://admin:bambabisli@cluster0.rpwhwel.mongodb.net/Recipes'
+const uri = `mongodb+srv://admin:${process.env.MONGODB_PASS}@cluster0.rpwhwel.mongodb.net/Recipes`
 
-app.listen(3030, () => console.log('* Server is listening on port', PORT))
+app.listen(PORT, () => console.log('* Server is listening on port', PORT))
 
 app.use(cors())
 app.use(Express.json())
